49ef4a6a71f0d149dfc69f31098c7e160f334c94
clx <1149302926@qq.com>
2022-03-29 18:57:21 +0800

    deploy

---
+++ precache-manifest.f5efe0031463fac5bb511053f8208ca9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bcbc974b0ad6ff844e7",
+    "url": "css/app.f78c72af.css"
+  },
+  {
+    "revision": "aa11d3b3a623da2b2281ad81415e44ba",
+    "url": "index.html"
+  },
+  {
+    "revision": "4bcbc974b0ad6ff844e7",
+    "url": "js/app.ed457f01.js"
+  },
+  {
+    "revision": "24deccd53e4f12b48bf7",
+    "url": "js/chunk-vendors.8445bdca.js"
+  },
+  {
+    "revision": "6ef63777adca36b8014bcf025fae0271",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "f138d1a70dc77d442db509d46af99bf7",
+    "url": "qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
